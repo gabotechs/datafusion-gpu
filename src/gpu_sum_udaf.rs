8be51ad4e08053f6043875c78973db8702376fb0
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2024-12-03 16:29:42 +0100

    Start executing code on GPU

---
+++ src/gpu_sum_udaf.rs
@@ -0,0 +1,200 @@
+use cubecl::prelude::Array as CubeArray;
+use cubecl::prelude::*;
+use datafusion::arrow::array::{Array, ArrayRef};
+use datafusion::arrow::datatypes::{DataType, Field, Float32Type};
+use datafusion::common::{exec_err, not_impl_err, Result, ScalarValue};
+use datafusion::functions_aggregate::sum::Sum;
+use datafusion::logical_expr::function::{AccumulatorArgs, StateFieldsArgs};
+use datafusion::logical_expr::utils::AggregateOrderSensitivity;
+use datafusion::logical_expr::{
+    Accumulator, AggregateUDF, AggregateUDFImpl, ReversedUDAF, Signature,
+};
+use delegate::delegate;
+use std::any::Any;
+use std::fmt::{Debug, Formatter};
+use std::sync::Arc;
+use tokio::time::Instant;
+
+pub fn udaf<R: Runtime>(compute_client: Arc<ComputeClient<R::Server, R::Channel>>) -> AggregateUDF {
+    AggregateUDF::from(GpuSum::<R> {
+        sum: Sum::default(),
+        compute_client,
+    })
+}
+
+#[derive(Debug)]
+struct GpuSum<R: Runtime> {
+    sum: Sum,
+    compute_client: Arc<ComputeClient<R::Server, R::Channel>>,
+}
+
+impl<R: Runtime> AggregateUDFImpl for GpuSum<R> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn name(&self) -> &str {
+        "sum_gpu"
+    }
+
+    fn return_type(&self, _: &[DataType]) -> Result<DataType> {
+        Ok(DataType::Float32)
+    }
+
+    fn accumulator(&self, args: AccumulatorArgs) -> Result<Box<dyn Accumulator>> {
+        let c = self.compute_client.clone();
+        let t = args.exprs[0].data_type(args.schema)?;
+        match t {
+            DataType::Int32 => Ok(Box::new(GpuSumAccumulator::<R>::new(c))),
+            DataType::UInt32 => Ok(Box::new(GpuSumAccumulator::<R>::new(c))),
+            DataType::Float32 => Ok(Box::new(GpuSumAccumulator::<R>::new(c))),
+            v => {
+                not_impl_err!("SumGpu not supported for {}: {}", args.name, v)
+            }
+        }
+    }
+
+    fn create_sliding_accumulator(&self, args: AccumulatorArgs) -> Result<Box<dyn Accumulator>> {
+        // TODO: does the same accumulator work?
+        self.accumulator(args)
+    }
+
+    // This function cannot be delegated to self.sum, because it will mess
+    // with the input types.
+    fn coerce_types(&self, arg_types: &[DataType]) -> Result<Vec<DataType>> {
+        if arg_types.len() != 1 {
+            return exec_err!("SUM expects exactly one argument");
+        }
+
+        Ok(vec![match &arg_types[0] {
+            dt if dt.is_signed_integer() => DataType::Int32,
+            dt if dt.is_unsigned_integer() => DataType::UInt32,
+            dt if dt.is_floating() => DataType::Float32,
+            _ => return exec_err!("Sum not supported for {}", arg_types[0]),
+        }])
+    }
+
+    delegate! {
+        to self.sum {
+            fn signature(&self) -> &Signature;
+            fn state_fields(&self, args: StateFieldsArgs) -> Result<Vec<Field>>;
+            // TODO: groups accumulators are not supported.
+            // fn groups_accumulator_supported(&self, args: AccumulatorArgs) -> bool;
+            // fn create_groups_accumulator( &self, args: AccumulatorArgs) -> Result<Box<dyn GroupsAccumulator>>;
+            fn aliases(&self) -> &[String];
+            fn order_sensitivity(&self) -> AggregateOrderSensitivity;
+            fn reverse_expr(&self) -> ReversedUDAF;
+        }
+    }
+}
+
+struct GpuSumAccumulator<R: Runtime> {
+    compute_client: Arc<ComputeClient<R::Server, R::Channel>>,
+    // TODO: Having a generic number here is very difficult, I still don't know how to do it, so I just use f32.
+    result: f32,
+}
+
+unsafe impl<R: Runtime> Send for GpuSumAccumulator<R> {}
+unsafe impl<R: Runtime> Sync for GpuSumAccumulator<R> {}
+
+impl<R: Runtime> GpuSumAccumulator<R> {
+    fn new(compute_client: Arc<ComputeClient<R::Server, R::Channel>>) -> Self {
+        Self {
+            compute_client,
+            result: 0.0,
+        }
+    }
+}
+
+impl<R: Runtime> Debug for GpuSumAccumulator<R> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "GpuSumAccumulator(?)",)
+    }
+}
+
+#[cube(launch_unchecked)]
+fn sum_basic<N: Numeric>(
+    input: &CubeArray<N>,
+    output: &mut CubeArray<N>,
+    #[comptime] end: Option<u32>,
+) {
+    let unroll = end.is_some();
+    let end = end.unwrap_or_else(|| input.len());
+
+    let mut sum = N::from_int(0);
+
+    #[unroll(unroll)]
+    for i in 0..end {
+        sum += input[i];
+    }
+
+    output[UNIT_POS] = sum;
+}
+
+impl<R: Runtime> Accumulator for GpuSumAccumulator<R> {
+    fn update_batch(&mut self, values: &[ArrayRef]) -> Result<()> {
+        let data_type = values[0].data_type();
+        let data = values[0].to_data();
+        let len = values[0].len();
+        let data: Vec<&[u8]> = data.buffers().iter().map(|b| b.as_slice()).collect();
+
+        let output = match data_type {
+            DataType::Int32 => self.compute_client.empty(size_of::<i32>()),
+            DataType::UInt32 => self.compute_client.empty(size_of::<u32>()),
+            DataType::Float32 => self.compute_client.empty(size_of::<f32>()),
+            v => return not_impl_err!("SumGpu not supported for {}", v),
+        };
+        let start = Instant::now();
+        let input = self.compute_client.create(data[0]);
+
+        let start = Instant::now();
+        unsafe {
+            macro_rules! run {
+                ($ty: ty) => {
+                    sum_basic::launch_unchecked::<$ty, R>(
+                        self.compute_client.as_ref(),
+                        CubeCount::Static(1, 1, 1),
+                        CubeDim::new(len as u32, 1, 1),
+                        ArrayArg::from_raw_parts(&input, len, 1),
+                        ArrayArg::from_raw_parts(&output, len, 1),
+                        Some(len as u32),
+                    )
+                };
+            }
+
+            match data_type {
+                DataType::Int32 => run!(i32),
+                DataType::UInt32 => run!(u32),
+                DataType::Float32 => run!(f32),
+                v => return not_impl_err!("SumGpu not supported for {}", v),
+            }
+        }
+
+        let bytes = self.compute_client.read(output.clone().binding());
+
+        let v = match data_type {
+            DataType::Int32 => i32::from_bytes(&bytes)[0] as f32,
+            DataType::UInt32 => u32::from_bytes(&bytes)[0] as f32,
+            DataType::Float32 => f32::from_bytes(&bytes)[0],
+            v => return not_impl_err!("SumGpu not supported for {}", v),
+        };
+        self.result += v;
+        Ok(())
+    }
+
+    fn evaluate(&mut self) -> Result<ScalarValue> {
+        ScalarValue::new_primitive::<Float32Type>(Some(self.result), &DataType::Float32)
+    }
+
+    fn size(&self) -> usize {
+        size_of_val(self)
+    }
+
+    fn state(&mut self) -> Result<Vec<ScalarValue>> {
+        Ok(vec![self.evaluate()?])
+    }
+
+    fn merge_batch(&mut self, states: &[ArrayRef]) -> Result<()> {
+        self.update_batch(states)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,118 @@
-fn main() {
-    println!("Hello, world!");
+mod gpu_sum_udaf;
+
+use clap::Parser;
+use cubecl::wgpu::WgpuRuntime;
+use cubecl::Runtime;
+use datafusion::arrow::array::{Float32Array, Int32Array, RecordBatch};
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::datasource::MemTable;
+use datafusion::execution::runtime_env::RuntimeEnv;
+use datafusion::execution::SessionStateBuilder;
+use datafusion::prelude::{CsvReadOptions, SessionConfig, SessionContext};
+use rand::Rng;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
+use std::sync::Arc;
+use tokio::time::Instant;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(default_value = "")]
+    sql: String,
+
+    #[arg(short, long, default_value = "1024")]
+    len: usize,
+}
+
+fn generate_random_numbers<T>(count: usize) -> Vec<T>
+where
+    rand::distributions::Standard: rand::distributions::Distribution<T>,
+{
+    let mut rng = rand::thread_rng();
+    (0..count).map(|_| rng.gen::<T>()).collect()
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let ctx = build_ctx(&args).await?;
+
+    let mut rl = DefaultEditor::new()?;
+    _ = rl.load_history(".history.txt");
+
+    if !args.sql.is_empty() {
+        let df = ctx.sql(&args.sql).await?;
+        rl.add_history_entry(&args.sql)?;
+        let start = Instant::now();
+        df.show().await?;
+        println!("Total execution time: {:?}", start.elapsed());
+    }
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line)?;
+                if line == "q" || line == "exit" || line == "quit" {
+                    return Ok(());
+                }
+                let df = match ctx.sql(&line).await {
+                    Ok(v) => v,
+                    Err(err) => {
+                        println!("{}", err);
+                        continue;
+                    }
+                };
+
+                let start = Instant::now();
+                df.show().await?;
+                println!("Total execution time: {:?}", start.elapsed());
+                println!()
+            }
+
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                return Ok(());
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                return Ok(());
+            }
+            Err(err) => return Err(err.into()),
+        };
+    }
+}
+
+async fn build_ctx(args: &Args) -> datafusion::error::Result<SessionContext> {
+    let config = SessionConfig::new();
+    let runtime = Arc::new(RuntimeEnv::default());
+    let state = SessionStateBuilder::new()
+        .with_default_features()
+        .with_config(config)
+        .with_runtime_env(runtime)
+        .build();
+
+    let compute_client = WgpuRuntime::client(&Default::default());
+    let ctx = SessionContext::new_with_state(state);
+
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("float", DataType::Float32, false),
+        Field::new("int", DataType::Int32, false),
+    ]));
+
+    let batch = RecordBatch::try_new(
+        schema.clone(),
+        vec![
+            Arc::new(Float32Array::from(generate_random_numbers::<f32>(args.len))),
+            Arc::new(Int32Array::from(generate_random_numbers::<i32>(args.len))),
+        ],
+    )?;
+
+    let table = MemTable::try_new(schema, vec![vec![batch]])?;
+    ctx.register_udaf(gpu_sum_udaf::udaf::<WgpuRuntime>(Arc::new(compute_client)));
+    ctx.register_table("numbers", Arc::new(table))?;
+    ctx.register_csv("test", "datasets/test.csv", CsvReadOptions::default())
+        .await?;
+    Ok(ctx)
 }
