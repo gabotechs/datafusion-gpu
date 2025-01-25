78b4d2fed26ecc4ca8dc2fe698c55745da38234a
sesh nalla <39490039+nerdsane@users.noreply.github.com>
2025-01-25 02:44:53 -0500

    improve performance with CubeCL (#1)
    * improve performance with CubeCL
    
    * removed debug statements + dependency clean up

--- src/cubecl_sum_udaf.rs
+++ src/cubecl_sum_udaf.rs
@@ -1,6 +1,4 @@
 use cubecl::prelude::*;
-use cubecl_reduce::reduce_plane;
-use cubecl_reduce::Sum as CubeSum;
 use datafusion::arrow::array::{Array, ArrayRef};
 use datafusion::arrow::datatypes::{DataType, Field, Float32Type};
 use datafusion::common::{exec_err, not_impl_err, Result, ScalarValue};
@@ -14,6 +12,8 @@ use delegate::delegate;
 use std::any::Any;
 use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
+use cubecl_reduce::reduce_plane;
+use cubecl_reduce::Sum as CubeSum;
 
 pub fn udaf<R: Runtime>(compute_client: Arc<ComputeClient<R::Server, R::Channel>>) -> AggregateUDF {
     AggregateUDF::from(GpuSum::<R> {
@@ -92,6 +92,7 @@ struct GpuSumAccumulator<R: Runtime> {
     compute_client: Arc<ComputeClient<R::Server, R::Channel>>,
     // TODO: Having a generic number here is very difficult, I still don't know how to do it, so I just use f32.
     result: f32,
+    has_processed: bool,
 }
 
 unsafe impl<R: Runtime> Send for GpuSumAccumulator<R> {}
@@ -102,6 +103,7 @@ impl<R: Runtime> GpuSumAccumulator<R> {
         Self {
             compute_client,
             result: 0.0,
+            has_processed: false,
         }
     }
 }
@@ -125,33 +127,51 @@ fn sum<N: Numeric>(
 
 impl<R: Runtime> Accumulator for GpuSumAccumulator<R> {
     fn update_batch(&mut self, values: &[ArrayRef]) -> Result<()> {
+        let len = values[0].len();
+
+        // Skip empty or invalid batches
+        if values.is_empty() || values[0].len() == 0 {
+            println!("Skipping empty batch");
+            return Ok(());
+        }
+
         let data_type = values[0].data_type();
         let data = values[0].to_data();
-        let len = values[0].len();
         let data: &[u8] = data.buffers().first().map(|b| b.as_slice()).unwrap();
 
-        let block_size = 1024;
-        let num_blocks = len.div_ceil(block_size);
+        let block_size = 256;
 
-        let output_handle = match data_type {
-            DataType::Int32 => self.compute_client.empty(size_of::<i32>()),
-            DataType::UInt32 => self.compute_client.empty(size_of::<u32>()),
-            DataType::Float32 => self.compute_client.empty(size_of::<f32>()),
+        const LINE_SIZE: u8 = 1;
+
+        let output_size = match data_type {
+            DataType::Int32 => size_of::<i32>(),
+            DataType::UInt32 => size_of::<u32>(),
+            DataType::Float32 => size_of::<f32>(),
             v => return not_impl_err!("SumGpu not supported for {}", v),
         };
+        
+        let output_handle = self.compute_client.empty(output_size);
         let input_handle = self.compute_client.create(data);
 
-        // TODO: Why this can only be 1?
-        const LINE_SIZE: u8 = 1;
         unsafe {
             macro_rules! run {
                 ($ty: ty) => {
                     sum::launch_unchecked::<$ty, R>(
-                        self.compute_client.as_ref(),
-                        CubeCount::new_1d(num_blocks as u32),
+                        self.compute_client.as_ref(),     
+                        CubeCount::new_1d(1),  // Single output
                         CubeDim::new_1d(block_size as u32),
-                        TensorArg::from_raw_parts::<$ty>(&input_handle, &[1], &[len], LINE_SIZE),
-                        TensorArg::from_raw_parts::<$ty>(&output_handle, &[1], &[1], LINE_SIZE),
+                        TensorArg::from_raw_parts::<$ty>(
+                            &input_handle, 
+                            &[1],
+                            &[len],
+                            LINE_SIZE
+                        ),
+                        TensorArg::from_raw_parts::<$ty>(
+                            &output_handle,
+                            &[1], 
+                            &[1],  // Single output value
+                            LINE_SIZE
+                        ),
                         ScalarArg::new(0),
                         block_size as u32,
                         false, // TODO: calc if the plane dim has the exact size of the data.
@@ -172,20 +192,15 @@ impl<R: Runtime> Accumulator for GpuSumAccumulator<R> {
             .read(vec![output_handle.clone().binding()]);
         let bytes = bytes.remove(0);
 
-        macro_rules! dump {
-            ($ty: ty) => {
-                for v in <$ty>::from_bytes(&bytes) {
-                    self.result += *v as f32;
-                }
-            };
-        }
-
-        match data_type {
-            DataType::Int32 => dump!(i32),
-            DataType::UInt32 => dump!(u32),
-            DataType::Float32 => dump!(f32),
+        let sum = match data_type {
+            DataType::Int32 => i32::from_bytes(&bytes)[0] as f32,
+            DataType::UInt32 => u32::from_bytes(&bytes)[0] as f32,
+            DataType::Float32 => f32::from_bytes(&bytes)[0],
             v => return not_impl_err!("SumGpu not supported for {}", v),
         };
+        
+        self.result += sum;
+        self.has_processed = true;
         Ok(())
     }
 
@@ -198,10 +213,21 @@ impl<R: Runtime> Accumulator for GpuSumAccumulator<R> {
     }
 
     fn state(&mut self) -> Result<Vec<ScalarValue>> {
+        if !self.has_processed {
+            return Ok(vec![ScalarValue::new_primitive::<Float32Type>(Some(0.0), &DataType::Float32)?]);
+        }
         Ok(vec![self.evaluate()?])
     }
 
     fn merge_batch(&mut self, states: &[ArrayRef]) -> Result<()> {
-        self.update_batch(states)
+        for state in states {
+            if let ScalarValue::Float32(Some(value)) = ScalarValue::try_from_array(state, 0)? {
+                if value != 0.0 {  // Only merge non-zero states
+                    self.result += value;
+                    self.has_processed = true;
+                }
+            }
+        }
+        Ok(())
     }
 }
